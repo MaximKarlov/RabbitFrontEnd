7360f8666db329dbf66f927025eae656118e4648
predator09071987@gmail.com <predator09071987@gmail.com>
2025-04-09 21:38:20 +0300

    add fix in rabbits.jsx in line13-15

--- src/pages/Rabbits.jsx
+++ src/pages/Rabbits.jsx
@@ -1,18 +1,18 @@
 import AppCss from '../components/App.module.css';
-// import { RabbitForm } from '../components/Form/Form';
-// import { Filter } from '../components/Filter/Filter';
+import { RabbitForm } from '../components/Form/Form';
+import { Filter } from '../components/Filter/Filter';
 import { RabbitList } from '../components/Rabbits/Rabbits';
 import { ButtonMenu } from '../components/btnMenu/btnMenu';
 
 export default function Main() {
   return (
-    //Щось гітхаб не прийняв зміни при перейменуванні файлу
     <>
+      //Щось гітхаб не прийняв зміни при перейменуванні файлу
       <div className={AppCss.component}>
-        {/* <h1>Rabbits</h1> */}
-        {/* <RabbitForm /> */}
-        {/* <h2>Contacts</h2> */}
-        {/* <Filter /> */}
+        <h1>Rabbits</h1>
+        <RabbitForm />
+        <h2>Contacts</h2>
+        <Filter />
         <RabbitList />
       </div>
       <div>
